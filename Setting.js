1fa7dbb5329727fd46505978f5f1b32eb2d661f7
Gabriel Hellebrand <hellebrandg@gmail.com>
2022-06-02 18:02:56 -0600

    Added stedi files

--- Setting.js
+++ Setting.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import { patchWebProps } from "react-native-elements/dist/helpers";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 
-function Setting() {
+function Setting(props) {
     return(
       <View>
+      <Text>Welcome {props.loggedInUser}</Text>
       <Button
       title="Log In"
       onPress={() => Alert.alert('Simple Button pressed')}
